812135983843605d27963305491219afe6332580
deepak chandh <deepak chandh>
2018-05-30 17:35:53 +0530

    added occurence

---
+++ occurence.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+#include<dos.h>
+#include<conio.h>
+using namespace std;
+int main() { 
+	int a=0;
+	while(a!=1111) {
+	a++;
+	printf("%5d",a);
+	delay(1000);
+	clrscr();
+		}
+		}
+
